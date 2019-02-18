e234ef5e4bc42769e257c20e87c5a1c675b8afa6
Steven <srs6p@mtmail.mtsu.edu>
2019-02-18 17:24:18 -0600

    Threaded

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn main() {
     client.with_framework(ZubotsuFramework::new());
 
     // start listening for events by starting a single shard
-    if let Err(why) = client.start() {
+    if let Err(why) = client.start_shards(8) {
         println!("An error occurred while running the client: {:?}", why);
     }
 }
