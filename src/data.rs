90587ea080be953774fe51dc25af48f71e75f27c
Steven <srs6p@mtmail.mtsu.edu>
2019-02-18 00:19:30 -0600

    Added dank readme

--- src/data.rs
+++ src/data.rs
@@ -1,3 +1,18 @@
+// Copyright 2019 Steven Sheffey
+// This file is part of Zubotsu.
+//
+// Foobar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Foobar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Zubotsu.  If not, see <https://www.gnu.org/licenses/>.
 pub const GNU_LINUX_COPYPASTA: &str = r#"I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
 
 Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called "Linux", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.
